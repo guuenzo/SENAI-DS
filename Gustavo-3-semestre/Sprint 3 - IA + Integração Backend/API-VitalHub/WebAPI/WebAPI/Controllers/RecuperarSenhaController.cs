9220f81790c12f815b4d6a0633c3e796afff46c9
Gustavo Enzo Santos Aguilar <49816214857@info.local>
2024-04-19 15:26:55 -0300

    .

---
+++ Gustavo-3-semestre/Sprint 3 - IA + Integração Backend/API-VitalHub/WebAPI/WebAPI/Controllers/RecuperarSenhaController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Contexts;
+using WebAPI.Domains;
+using WebAPI.Utils.Mail;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecuperarSenhaController : ControllerBase
+    {
+        private readonly VitalContext _context;
+        private readonly EmailSendingService _emailSendingService;
+
+
+        public RecuperarSenhaController(VitalContext context, EmailSendingService emailSendingService)
+        {
+            _context = context;
+            _emailSendingService = emailSendingService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendRecoveryCodePassword(string email)
+        {
+            try
+            {
+                var user = await _context.Usuarios.FirstOrDefaultAsync(x => x.Email == email);
+
+                if (user == null)
+                {
+                    return NotFound("Usuário não encontrado");
+                }
+                //gerar um codigo com 4 algarismos
+                Random random = new Random();
+                int recoveryCode = random.Next(1000, 9999);
+
+                user.CodRecupSenha = recoveryCode;
+
+                await _context.SaveChangesAsync();
+
+                await _emailSendingService.SendRecovery(user.Email, recoveryCode);
+
+                return (Ok("Código enviado com sucesso"));
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+        }
+
+        //CRIE UM CONTROLLER PARA VALIDAR O CODIGO ENVIADO PARA O EMAIL
+        //SE O CODIGO FOR IGUAL, RESETE O CODIGO ANTERIOR NO BANCO E DEVOLVA UM STATUS CODE INFORMANDO SE O CODIGO E INVALIDO
+        [HttpPost("Verificar código")]
+        public async Task<IActionResult> VerifyRecoveryCodePassword(int codigo)
+        {
+            try
+            {
+                var user = await _context.Usuarios.FirstOrDefaultAsync(x => x.CodRecupSenha == codigo);
+
+                if (user == null)
+                {
+                    return (Ok("Usuário encontrado"));
+                }
+
+                if (user.CodRecupSenha != codigo)
+                {
+                    return (BadRequest("Código inválido"));
+                }
+
+                user.CodRecupSenha = null;
+
+                await _context.SaveChangesAsync();
+
+                return (NotFound("Código válido"));
+            }
+            catch (Exception erro)
+            {
+                return (NotFound(erro.Message));
+            }
+        }
+    }
+}
---
+++ Gustavo-3-semestre/Sprint 3 - IA + Integração Backend/API-VitalHub/WebAPI/WebAPI/Utils/OCR/OcrService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+namespace WebAPI.Utils.OCR
+{
+    public class OcrService
+    {
+        private readonly string _subscriptKey = "dbecd37085bc4627b2c47eb291676357";
+
+        private readonly string _endpoint = "https://cvvitalhubg16enzoo.cognitiveservices.azure.com/";
+
+        public async Task<string> RecognizeTextAsync(Stream imageStream)
+        {
+            try
+            {
+                var client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(_subscriptKey))
+                {
+                    Endpoint = _endpoint
+                };
+
+                var ocrResult = await client.RecognizePrintedTextInStreamAsync(true, imageStream);
+
+                return ProcessRecognitionResult(ocrResult);
+            }
+            catch (Exception erro)
+            {
+                return erro.Message;
+            }
+        } 
+        private static string ProcessRecognitionResult(OcrResult result)
+        {
+            try
+            {
+                string recognizedText = "";
+
+                foreach (var region in result.Regions)
+                {
+                    foreach (var line in region.Lines)
+                    {
+                        foreach (var word in line.Words)
+                        {
+                            recognizedText += word.Text + " ";
+                        }
+                        return recognizedText += "\n";
+                    }
+                }
+                return recognizedText;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+    }
+}
